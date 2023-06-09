850a21f3bbd374f8b5a512529ef77379acd38ed9
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-06-09 17:46:45 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,38 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!!" />
-        <p className="description">
-          Get started by editing <code>print:("hello world")</code>
-          
-        </p>
-        <p className="description">
-          welcome to my app!
-          
-        </p>
-       
-      </main>
+  <main>
+    <Header title="Welcome to my app!" />
+    <p className="description">
+
+      Get started by editing <code>pages/index.js</code>
+
+   <p className="description">   
+    <strong>Emmanuel Yee Hong Wei</strong> </p>
+
+
+
+    <p className="description">
+
+
+        TechnoBear is a website dedicated to showcasing the work of TechnoBear. You can find his latest projects, news and updates here. 
+
+    </p>
+    <p className="social-links">       
+    <a href="https://www.facebook.com/example">Facebook</a> | <a href="https://twitter.com/example">Twitter</a> | <a href="https://www.instagram.com/example">Instagram
+    </a>     
+    </p>   
+
+
+
+  </main>
+
+  <Footer />
+</div>
+)
+}
+
+
 
       <Footer />
     </div>
