d655757567723e7498f41d14880b6e301e318357
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-06-10 11:47:58 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Emmanuel Yee </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
     <main>
-    <Header title="Welcome to my app!" />
+    <Header title="Emmanuel Yee Hong Wei" />
     <p className="description">
 
       Get started by editing <code>pages/index.js</code>
@@ -19,7 +19,7 @@ export default function Home() {
     </p>
     <p className="description">
 
-      welcome to my app!
+      Welcome to my app!
 
     </p>
 
